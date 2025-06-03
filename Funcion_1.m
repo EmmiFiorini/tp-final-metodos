d12d74d892ca326ce241e106bf5ac5f78d7b4b9e
tinavidaurreta <142612257+tinavidaurreta@users.noreply.github.com>
2025-06-03 16:19:57 -0300

    Punto 3 hasta el f

---
+++ Funcion_1.m
@@ -0,0 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%
+%%% Función para el ejercicio 3
+%%% Usado por el Ejercicio Nro. 3
+%%%
+%%% TP#FINAL Métodos Numéricos
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function y = Funcion_1(X,A,B)
+
+ y = A * X + B;
+
+end
