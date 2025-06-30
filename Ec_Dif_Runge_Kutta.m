cb079047a4c5be2104535b9a8471bbe433651484
EmmiFiorini <143649576+EmmiFiorini@users.noreply.github.com>
2025-06-30 17:24:00 -0300

    jdjd

---
+++ Ec_Dif_Runge_Kutta.m
@@ -0,0 +1,53 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%
+%%% Ejercicio Nro. 12 TP#2 Métodos Numéricos
+%%%
+%%% Resolución de Sistemas Ecuaciones Diferenciales de primer Orden:
+%%%         x1'=f1(t,x1,x2,...,xn)
+%%%         x2'=f2(t,x1,x2,...,xn)
+%%%         x3'=f3(t,x1,x2,...,xn)
+%%%              :   :   :
+%%%         xn'=fn(t,x1,x2,...,xn)
+%%%
+%%% Método de RUNGE-KUTTA Orden 4
+%%%
+%%% function [T,X] = Ec_Dif_Runge_Kutta_O4_Sistemas(F,t0,tf,X0,M)
+%%%
+%%% Parámetros de Entrada:
+%%%       f  = f(t,y) ingresada como cadena (script)
+%%%       t0 = Tiempo inicial de análisis de la Ecuacion Diferencial
+%%%       tf = Tiempo final de análisis de la Ecuacion Diferencial
+%%%       y0 = y(t0) Condicion inicial de la ED de primer orden
+%%%       M = Número de pasos del método
+%%%
+%%% Parámetros de Salida:
+%%%
+%%% Matiz E = [T Y], siendo: 
+%%%         T = Vector Columna Tiempo
+%%%         Y = Vector Columna Solución de la Ecuación Diferencial
+%%%
+%%% Dr. Ing. Franco Pessana
+%%% FICEN
+%%% Universidad Favaloro
+%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function E = Ec_Dif_Runge_Kutta(F,t0,tf,y0,M)
+
+if nargin<5
+    disp('Debe ingresar función f, tiempos iniciales y finales t0, tf, condicion inicial y0 y pasos M');
+    return;
+end
+
+h=(tf-t0)/M; % Intervalo de tiempo Delta_t
+T=t0:h:tf;  % Vector de Tiempo (M+1 valores)
+Y=zeros(size(T));
+Y(1)=y0;    
+for k=1:M
+    f1=feval(F,T(k),Y(k));
+    f2=feval(F,T(k)+h/2,Y(k)+h*f1/2);
+    f3=feval(F,T(k)+h/2,Y(k)+h*f2/2);
+    f4=feval(F,T(k)+h,Y(k)+h*f3);
+    Y(k+1)=Y(k)+h*(f1+2*f2+2*f3+f4)/6;
+end
+E=[T' Y'];
